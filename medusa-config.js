e4e9a255c45b27baf3683c530e652ec398c46642
Bernard Bolter <bernardbolter@gmail.com>
2025-11-21 16:25:15 +0100

    updated medusa config to js syntax

--- medusa-config.js
+++ medusa-config.js
@@ -1,15 +1,14 @@
-// DEBUG: THIS IS THE FIXED CONFIG – Fri Nov 21 14:05:48 CET 2025
-import { defineConfig } from '@medusajs/framework/utils'
-
-module.exports = defineConfig({
+module.exports = {
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+      storeCors: process.env.STORE_CORS || "",
+      adminCors: process.env.ADMIN_CORS || "",
+      authCors: process.env.AUTH_CORS || "",
+      jwtSecret: process.env.JWT_SECRET,
+      cookieSecret: process.env.COOKIE_SECRET,
+    },
+  },
+  // Optional: add plugins, modules, etc. here if you have them
+  // plugins: [],
+}
